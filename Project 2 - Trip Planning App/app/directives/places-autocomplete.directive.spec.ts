af4c45ea52660cb2aede8409a832b713c8e3a14f
Zachary Ritchie <zritchie343@gmail.com>
2018-10-22 09:27:21 -0400

    Last changes

--- Project 2 - Trip Planning App/app/directives/places-autocomplete.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlacesAutocompleteDirective } from './places-autocomplete.directive';
-
-describe('PlacesAutocompleteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PlacesAutocompleteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
